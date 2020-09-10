f6b0415bf42cccddcccb8cdda0bf129c1f025e84
LaisLuderer <38754177+LaisLuderer@users.noreply.github.com>
2020-09-09 21:41:25 -0300

    Update transaction.routes.ts

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactionRegister = transactionsRepository.all();
+    const transactionBalance = transactionsRepository.getBalance();
+    return response.json({
+      transactionRegister,
+      transactionBalance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +22,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // dados para criar a transaction
+    const { title, value, type } = request.body;
+
+    const CreateTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+    // aplicar as regras de negocio do service -> metodo execute
+    const transaction = CreateTransaction.execute({
+      title,
+      value,
+      type,
+    });
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
